795c2b53e3f303bc818417533ca1ee7c9e37638c
mrasilbek19 <asilbek190303@gmail.com>
2026-08-03 22:43:32 +0900

    feat: develop admin controller and testing routing

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import restaurantController from "./controllers/member.controller";
 const routerAdmin = express.Router();
+
+routerAdmin.get("/", restaurantController.goHome);
+
 
 export default routerAdmin;
