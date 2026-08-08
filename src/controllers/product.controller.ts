8d748b8cc913196db4db5ca6ccf6e526284a5a6e
mrasilbek19 <asilbek190303@gmail.com>
2026-08-08 17:30:57 +0900

    feat: develop product types and product service model

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,13 @@
 import { T } from "../libs/types/common";
+import ProductService from "../models/Product.service";
 
-
+const productService = new ProductService();
 
 const productController: T = {}
+
+/** SSR **/
+
+
+
+
+/** SPA **/
---
+++ src/libs/types/product.ts
@@ -0,0 +1,57 @@
+import { ObjectId } from "mongoose";
+import {
+    ProductCollection,
+    ProductStatus
+} from "../enums/product.enum";
+
+
+export interface Product {
+    _id: ObjectId;
+    productStatus: ProductStatus;
+    productCollection: ProductCollection;
+    productName: string;
+    productPrice: number;
+    productLeftCount: number;
+    productVolume: number;
+    productDecs?: string;
+    productImages: string[];
+    productViews: number;
+    productLikes: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ProductInquiry {
+    order: string;
+    page: number;
+    limit: number;
+    productCollection?: ProductCollection;
+    search?: string;
+}
+
+export interface ProductInput {
+    productStatus?: ProductStatus;
+    productCollection: ProductCollection;
+    productName: string;
+    productPrice: number;
+    productLeftCount: number;
+    productVolume?: number;
+    productDecs?: string;
+    productImages?: string[];
+    productViews?: number;
+    productLikes?: number;
+}
+
+export interface ProductUpdateInput {
+    _id: ObjectId;
+    productStatus?: ProductStatus;
+    productCollection?: ProductCollection;
+    productName?: string;
+    productPrice?: number;
+    productLeftCount?: number;
+    productVolume?: number;
+    productDecs?: string;
+    productImages?: string[];
+    productViews?: number;
+    productLikes?: number;
+}
