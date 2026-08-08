0115d394d3e713f32feee7609fc466d3b33000a3
mrasilbek19 <asilbek190303@gmail.com>
2026-08-08 12:50:25 +0900

    feat: develop get Shop info rest api

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -10,6 +10,22 @@ const authService = new AuthService();
 const memberService = new MemberService();
 const memberController: T = {};
 
+
+memberController.getShop = async (req: Request, res: Response) => {
+    try {
+        console.log("getShop page");
+
+        const result = await memberService.getShop();
+        console.log(result);
+
+        res.status(HttpCode.OK).json(result);
+    } catch (err) {
+        console.log("Error, getShop:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+}
+
 memberController.signup = async (req: Request, res: Response) => {
     try {
         console.log("signup page")
@@ -27,9 +43,6 @@ memberController.signup = async (req: Request, res: Response) => {
         else res.status(Errors.standard.code).json(Errors.standard);
     }
 };
-
-
-
 
 
 export default memberController;
--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,18 @@ import express from "express";
 import memberController from "./controllers/member.controller";
 const router = express.Router();
 
+/* MEMBER */
 
+router.get("/member/shop", memberController.getShop)
 router.post("/member/signup", memberController.signup);
+
+
+/* PRODUCT */
+
+
+
+
+/* USER */
+
 
 export default router;
